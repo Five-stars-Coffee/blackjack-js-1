2d60e48fe79896742cdc78edb3bf46e6e5068b19
Martin Bean <martin@martinbean.co.uk>
2013-07-14 19:53:34 +0100

    Added basic gameplay logic printed out to console

--- assets/js/blackjack.js
+++ assets/js/blackjack.js
@@ -2,11 +2,25 @@
 var Card = function(value, suit) {
     this.value = value;
     this.suit = suit;
+    this.getValue = function() {
+        return (this.value > 10) ? 10 : this.value;
+    };
 }
 
 var Player = function(name) {
     this.name = name;
     this.hand = [];
+    this.handTotal = 0;
+    this.calculateHandTotal = function() {
+        this.handTotal = 0;
+        for (var i = 0; i < this.hand.length; i++) {
+            this.handTotal += (this.hand[i].value > 10) ? 10 : this.hand[i].value;
+        }
+    };
+    this.isBust = function() {
+        this.calculateHandTotal();
+        return (this.handTotal > 21);
+    };
 }
 
 var deck = [];
@@ -37,14 +51,72 @@ for (var i = 0; i < players.length; i++) {
 
 // assign dealer and players their seconds cards
 dealer.hand.push(deck.pop());
+dealer.calculateHandTotal();
 
 for (var i = 0; i < players.length; i++) {
     players[i].hand.push(deck.pop());
+    players[i].calculateHandTotal();
 }
 
-console.log(deck, dealer, players);
+console.log('Player’s hand total: ' + players[0].handTotal);
 
 // each player gets turn (hit, stick)
+$('.hit').on('click', function(e) {
+    e.preventDefault();
+    players[0].hand.push(deck.pop());
+    players[0].calculateHandTotal();
+    console.log('Player hit');
+    console.log('Player’s new hand total: ' + players[0].handTotal);
+    if (players[0].handTotal > 20) {
+        if (players[0].handTotal > 21) {
+            console.log('Player bust');
+        }
+        else {
+            console.log('Player has 21; moving to next player');
+        }
+        dealersTurn();
+    }
+});
+
+$('.stick').on('click', function(e) {
+    e.preventDefault();
+    console.log('Player stuck');
+    dealersTurn();
+});
+
 // when end player reached, dealer gets turn
-// after dealer’s turn, calculate results
-// rinse and repeat
+function dealersTurn() {
+    console.log('Dealer’s turn');
+    console.log('Dealer’s hand total: ' + dealer.handTotal);
+    while (dealer.handTotal < 18) {
+        dealer.hand.push(deck.pop());
+        dealer.calculateHandTotal();
+        console.log('Dealer’s new hand total: ' + dealer.handTotal);
+    }
+    
+    // calculate results
+    console.log('Dealer’s hand total at end of game: ' + dealer.handTotal);
+    console.log('Dealer bust: ' + dealer.isBust());
+    
+    for (var i = 0; i < players.length; i++) {
+        console.log('Player ' + i + '’s hand total at end of game: ' + players[i].handTotal);
+        console.log('Player ' + i + ' bust: ' + players[i].isBust());
+        
+        // if player is not bust and player’s hand is greater than dealer’s, player wins
+        if (!players[i].isBust() && players[i].handTotal > dealer.handTotal) {
+            console.log('Player ' + i + ' wins');
+            // here we would pay out to player
+        }
+        else {
+            console.log('Player ' + i + ' loses');
+            if (dealer.isBust()) {
+                // dealer is bust, so return stakes to players
+            }
+            else {
+                // dealer is still in the game so takes players’ stakes
+            }
+        }
+    }
+    
+    console.log('Game over');
+}
